489b261b0d5835d02834c471b99d1d683c17d445
xanity-07 <th3loot69@gmail.com>
2025-01-08 12:49:39 -0500

    added head and styling

---
+++ src/components/activityGraph/ActivityGraph.jsx
@@ -0,0 +1,7 @@
+export default function ActivityGraph() {
+  return (
+    <div>
+      <h2>#######</h2>
+    </div>
+  );
+}
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,11 @@
 import './index.css';
+import Logo from '/Users/xan1ty/Desktop/fitness-tracker-version2/fitness-tracker/fitness-tracker-v2/src/assets/weight.png';
 
 export default function Header() {
   return (
-    <headerv className='header'>
+    <header className='header'>
+      <img src={Logo} alt='logo' />
       <h1>Fitness Tracker V2</h1>
-    </headerv>
+    </header>
   );
 }
