720f05cfd8041d54000c3a2d3ece4602881a1da9
xanity-07 <th3loot69@gmail.com>
2025-02-11 12:06:22 -0500

    deleted all components starting over

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,4 @@
-import { useState } from 'react';
-import Header from './components/header/Header';
-import ActivityGraph from './components/activityGraph/ActivityGraph';
-
 function App() {
-  const year = 2025;
-  const activityData = {
-    '2025-01-01': 6,
-    '2025-01-02': 14,
-    '2025-01-05': 14,
-    '2025-01-07': 9,
-    '2025-01-08': 21,
-    '2025-01-09': 14,
-    '2025-01-12': 21,
-    '2025-01-14': 9,
-    '2025-01-15': 14,
-    '2025-01-16': 14,
-    '2025-01-18': 21,
-  };
-
-  return (
-    <>
-      <Header />
-      <ActivityGraph year={year} activityData={activityData} />
-    </>
-  );
+  return <h1> Hello </h1>;
 }
 export default App;
